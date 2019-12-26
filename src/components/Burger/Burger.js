b9d6156ec630dd41197df56d1e159d4aa2d71084
Steven <stevenyan53@gmail.coim>
2019-12-26 22:46:05 +1300

    display the burger dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,16 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props)=>{
+    const transformedIngredient = Object.keys(props.ingredients)
+    .map(igKey =>{
+        return [...Array(props.ingredients[igKey])].map((_,i)=>{
+           return   <BurgerIngredient key={igKey +i} type ={igKey}/>
+        })
+    });
     return (
         <div className ={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="salad" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
