6d8bcb8eeaf1338876b443abc61cb8eabf4de74b
Steven <stevenyan53@gmail.coim>
2019-12-27 20:31:27 +1300

    Summary only appears when checkout is clicked

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
             meat:0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients){
@@ -76,7 +77,9 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     };
     
-
+    purchaseHandler =() =>{
+        this.setState({purchasing: true});
+    }
 
     render(){
         const disabledInfo ={
@@ -87,7 +90,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal>
+                <Modal show ={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients ={this.state.ingredients}/>
@@ -97,6 +100,7 @@ class BurgerBuilder extends Component {
                     disabled = {disabledInfo}
                     price ={this.state.totalPrice}
                     purchasable ={this.state.purchasable}
+                    ordered = {this.purchaseHandler}
                     />
 
             </Aux>
